edf0ca92749b24c1d22ceb45dca6c723b14a8da6
hansenjl <jenn.leigh.hansen@gmail.com>
2020-08-09 21:34:36 -0700

    oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
     has_secure_password
 
-    def self.from_omniauth(auth_response)
-        User.find_or_create_by(uid: auth_response[:uid]) do |u|
-            u.username = auth_response[:info][:name]
-            u.email = auth_response[:info][:email]
+    def self.from_omniauth(response)
+        User.find_or_create_by(uid: response[:uid], provider: response[:provider]) do |u|
+            u.username = response[:info][:name]
+            u.email = response[:info][:email]
             u.password = SecureRandom.hex(15)
         end
     end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   root 'sessions#welcome'
   resources :users
   resources :parties, only: [:index,:show]
+
+
   get '/auth/:provider/callback' => 'sessions#omniauth'
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
